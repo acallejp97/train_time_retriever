bb5cd610465b2cbca268c40f14b83944eaacd914
Asier Callejo <asiercallejo20@gmail.com>
2024-10-09 16:19:14 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
-crontab -l|sed "\$a$CRON_ONE_WAY python3 /usr/src/app/main.py $ORIGIN $DESTINATION"|crontab -
-crontab -l|sed "\$a$CRON_RETURN_1 python3 /usr/src/app/main.py $DESTINATION $ORIGIN"|crontab -
-crontab -l|sed "\$a$CRON_RETURN_2 python3 /usr/src/app/main.py $DESTINATION $ORIGIN"|crontab 
+
+echo "Configuring cron jobs..."
+
+echo "$CRON_ONE_WAY root python3 /usr/src/app/main.py $ORIGIN $DESTINATION >> /var/log/cron.log 2>&1" > /etc/cron.d/cron_execution_1
+echo "$CRON_RETURN_1 root python3 /usr/src/app/main.py $DESTINATION $ORIGIN >> /var/log/cron.log 2>&1" > /etc/cron.d/cron_execution_2
+echo "$CRON_RETURN_2 root python3 /usr/src/app/main.py $DESTINATION $ORIGIN >> /var/log/cron.log 2>&1" > /etc/cron.d/cron_execution_3
+
+chmod 0644 /etc/cron.d/cron_execution_1
+chmod 0644 /etc/cron.d/cron_execution_2
+chmod 0644 /etc/cron.d/cron_execution_3
+
+cat /etc/cron.d/cron_execution_1
+cat /etc/cron.d/cron_execution_2
+cat /etc/cron.d/cron_execution_3
+
+echo "Starting cron..."
+crond -l 2 && tail -f /var/log/cron.log
