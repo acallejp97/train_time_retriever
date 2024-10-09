0f4c55a729e869c1d22a57723ba9afdbd7781ecc
Asier Callejo <asiercallejo20@gmail.com>
2024-10-09 15:56:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Crear el cronjob con la variable de entorno CRON
-echo "$CRON_ONE_WAY python3 /usr/src/app/main.py $ORIGIN $DESTINATION>> /var/log/cron.log 2>&1" > /etc/cron.d/cron_execution_1
-echo "$CRON_RETURN_1 python3 /usr/src/app/main.py $DESTINATION $ORIGIN>> /var/log/cron.log 2>&1" > /etc/cron.d/cron_execution_2
-echo "$CRON_RETURN_2 python3 /usr/src/app/main.py $DESTINATION $ORIGIN>> /var/log/cron.log 2>&1" > /etc/cron.d/cron_execution_3
+echo "$CRON_ONE_WAY python3 /usr/src/app/main.py $ORIGIN $DESTINATION >> /var/log/cron.log 2>&1" > /etc/cron.d/cron_execution_1
+echo "$CRON_RETURN_1 python3 /usr/src/app/main.py $DESTINATION $ORIGIN >> /var/log/cron.log 2>&1" > /etc/cron.d/cron_execution_2
+echo "$CRON_RETURN_2 python3 /usr/src/app/main.py $DESTINATION $ORIGIN >> /var/log/cron.log 2>&1" > /etc/cron.d/cron_execution_3
 
 # Otorgar permisos correctos
 chmod 0644 /etc/cron.d/cron_execution_1
